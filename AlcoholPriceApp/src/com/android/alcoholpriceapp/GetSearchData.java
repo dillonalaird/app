343aad0392dab6f7c42a7e8733b1e5ce1abb585c
Troy Cosentino <tcosentino@zagmail.gonzaga.edu>
2012-12-18 19:52:50 -0800

    GetSearchData.java added

---
+++ AlcoholPriceApp/src/com/android/alcoholpriceapp/GetSearchData.java
@@ -0,0 +1,47 @@
+package com.android.alcoholpriceapp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+
+public class GetSearchData extends AsyncTask<String, Integer, String> {
+
+	//first argument is the name
+	//second argument is the size
+	@Override
+	protected String doInBackground(String... params) {
+		BufferedReader in = null;
+		String data = null;
+		
+		HttpGet request = new HttpGet("http://easyuniv.com/API/alc/" + params[0] + "/" + params[1]);
+		DefaultHttpClient client = new DefaultHttpClient();
+		try {
+			HttpResponse response = client.execute(request);
+			in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String l = "";
+			String nl = System.getProperty("line.separator");
+			while((l = in.readLine()) != null) {
+				sb.append(l + nl);
+			}
+			in.close();
+			data = sb.toString();
+		} catch(Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			if(in != null) {
+				try {
+					in.close();
+				} catch(Exception e) {
+					//do something?
+				}
+			}
+		}
+		return data;
+	}
+}
